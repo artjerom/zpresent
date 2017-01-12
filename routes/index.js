2d1c27b9eb4d8eda759e69360a827c32749deb1a
artjerom <artjerom@gmail.com>
2017-01-12 18:18:38 +0300

    80%

--- routes/index.js
+++ routes/index.js
@@ -4,23 +4,11 @@ var router = express.Router();
 var dataPresent = require(__dirname + '/../public/data/presentations.json');
 
 /* GET home page. */
-
-router.get('/', function (req, res) {});
-
-router.get('/present/:name', function(req, res) {
-  var model = dataPresent;
-
-  model.find(function () {
-    var name = req.params.name;
-    console.log(name);
-  });
+router.get('/', function(req, res) {
 
   res.render('presentation', {
     title: "Презентация",
-    name: req.params.name,
-    data: model.find(function () {
-      return name = req.params.name;
-    })
+    data: dataPresent
   });
 });
 
